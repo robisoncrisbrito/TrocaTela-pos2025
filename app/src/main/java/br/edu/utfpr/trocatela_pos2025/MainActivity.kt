b44534bf76d3f87c3b785c8f21440a9602d78c29
robisoncrisbrito <robison.brito@gmail.com>
2025-03-15 14:51:20 -0300

    programa troca de tela simples (da primeira para segunda tela)

--- app/src/main/java/br/edu/utfpr/trocatela_pos2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela_pos2025/MainActivity.kt
@@ -1,11 +1,9 @@
 package br.edu.utfpr.trocatela_pos2025
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun btLancamentoOnClick(view: View) {
-        //troca tela
+        val intent = Intent( this, LancamentoActivity::class.java )
+        startActivity( intent )
     }
 }
